41a6ced070ddf9866b0a2bc8a5174b440a2d0d07
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-10-10 17:30:56 +0200

    Match the current OS scheme color to set the page color; if no localstorage value is found

--- src/color/index.ts
+++ src/color/index.ts
@@ -1,5 +1,9 @@
 const color_raw = localStorage.getItem("color");
 const color: Color =
-	color_raw === "light" || color_raw === "dark" ? color_raw : "light";
+	color_raw === "light" || color_raw === "dark"
+		? color_raw
+		: window.matchMedia("(prefers-color-scheme: dark)").matches
+		? "dark"
+		: "light";
 
 document.documentElement.classList.add(`tamachi-color-${color}`);
